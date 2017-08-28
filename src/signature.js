920228960abc4ec00408d1fcece4019246272da9
robMaher84 <robmaheremail@gmail.com>
2017-08-28 15:02:51 -0400

    sig field custom edits

--- src/signature.js
+++ src/signature.js
@@ -5,16 +5,27 @@
 
 angular.module('signature', []);
 
-angular.module('signature').directive('signaturePad', ['$interval', '$timeout', '$window',
+angular.module('smartApp').directive('signaturePad', ['$interval', '$timeout', '$window',
   function ($interval, $timeout, $window) {
     'use strict';
 
-    var signaturePad, element, EMPTY_IMAGE = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAjgAAADcCAQAAADXNhPAAAACIklEQVR42u3UIQEAAAzDsM+/6UsYG0okFDQHMBIJAMMBDAfAcADDATAcwHAAwwEwHMBwAAwHMBzAcAAMBzAcAMMBDAcwHADDAQwHwHAAwwEMB8BwAMMBMBzAcADDATAcwHAADAcwHADDAQwHMBwAwwEMB8BwAMMBDAfAcADDATAcwHAAwwEwHMBwAAwHMBzAcAAMBzAcAMMBDAcwHADDAQwHwHAAwwEwHMBwAMMBMBzAcAAMBzAcwHAADAcwHADDAQwHMBwAwwEMB8BwAMMBDAfAcADDATAcwHAAwwEwHMBwAAwHMBzAcCQADAcwHADDAQwHwHAAwwEMB8BwAMMBMBzAcADDATAcwHAADAcwHMBwAAwHMBwAwwEMBzAcAMMBDAfAcADDAQwHwHAAwwEwHMBwAAwHMBzAcAAMBzAcAMMBDAcwHADDAQwHwHAAwwEMB8BwAMMBMBzAcADDATAcwHAADAcwHMBwAAwHMBwAwwEMB8BwAMMBDAfAcADDATAcwHAAwwEwHMBwAAwHMBzAcAAMBzAcAMMBDAcwHADDAQwHwHAAwwEMB8BwAMMBMBzAcADDkQAwHMBwAAwHMBwAwwEMBzAcAMMBDAfAcADDAQwHwHAAwwEwHMBwAMMBMBzAcAAMBzAcwHAADAcwHADDAQwHMBwAwwEMB8BwAMMBMBzAcADDATAcwHAADAcwHMBwAAwHMBwAwwEMBzAcAMMBDAegeayZAN3dLgwnAAAAAElFTkSuQmCC';
+    var EMPTY_IMAGE = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA'+
+    'jgAAADcCAQAAADXNhPAAAACIklEQVR42u3UIQEAAAzDsM+/6UsYG0okFDQHMBIJAMMBDAfAcADDATAcwHAAwwEwH'+
+    'MBwAAwHMBzAcAAMBzAcAMMBDAcwHADDAQwHwHAAwwEMB8BwAMMBMBzAcADDATAcwHAADAcwHADDAQwHMBwAwwEMB'+
+    '8BwAMMBDAfAcADDATAcwHAAwwEwHMBwAAwHMBzAcAAMBzAcAMMBDAcwHADDAQwHwHAAwwEwHMBwAMMBMBzAcAAMB'+
+    'zAcwHAADAcwHADDAQwHMBwAwwEMB8BwAMMBDAfAcADDATAcwHAAwwEwHMBwAAwHMBzAcCQADAcwHADDAQwHwHAAw'+
+    'wEMB8BwAMMBMBzAcADDATAcwHAADAcwHMBwAAwHMBwAwwEMBzAcAMMBDAfAcADDAQwHwHAAwwEwHMBwAAwHMBzAc'+
+    'AAMBzAcAMMBDAcwHADDAQwHwHAAwwEMB8BwAMMBMBzAcADDATAcwHAADAcwHMBwAAwHMBwAwwEMB8BwAMMBDAfAc'+
+    'ADDATAcwHAAwwEwHMBwAAwHMBzAcAAMBzAcAMMBDAcwHADDAQwHwHAAwwEMB8BwAMMBMBzAcADDkQAwHMBwAAwHM'+
+    'BwAwwEMBzAcAMMBDAfAcADDAQwHwHAAwwEwHMBwAMMBMBzAcAAMBzAcwHAADAcwHADDAQwHMBwAwwEMB8BwAMMBM'+
+    'BzAcADDATAcwHAADAcwHMBwAAwHMBwAwwEMBzAcAMMBDAegeayZAN3dLgwnAAAAAElFTkSuQmCC';
 
     return {
       restrict: 'EA',
       replace: true,
-      template: '<div class="signature" style="width: 100%; max-width:{{width}}px; height: 100%; max-height:{{height}}px;"><canvas style="display: block; margin: 0 auto;" ng-mouseup="onMouseup()" ng-mousedown="notifyDrawing({ drawing: true })"></canvas></div>',
+      template: '<div class="signature" style="width: 100%; max-width:{{width}}px; height: 100%;'+
+      ' max-height:{{height}}px;"><canvas style="display: block; margin: 0 auto;"'+
+      'ng-mouseup="onMouseup()" ng-mouseleave="onMouseup()"></canvas></div>',
       scope: {
         accept: '=?',
         clear: '=?',
@@ -22,42 +33,44 @@ angular.module('signature').directive('signaturePad', ['$interval', '$timeout', 
         dataurl: '=?',
         height: '@',
         width: '@',
+        fieldForm: '=',
+        fieldId: '=',
         notifyDrawing: '&onDrawing',
+        displayOnly: '='
       },
       controller: [
         '$scope',
         function ($scope) {
-          $scope.accept = function () {
 
+          $scope.accept = function () {
             return {
               isEmpty: $scope.dataurl === EMPTY_IMAGE,
               dataUrl: $scope.dataurl
             };
           };
 
+          $scope.dataurl = $scope.fieldForm[$scope.fieldId];
+          $scope.dataUrl = $scope.dataurl;
+
           $scope.onMouseup = function () {
             $scope.updateModel();
-
-            // notify that drawing has ended
             $scope.notifyDrawing({ drawing: false });
           };
 
           $scope.updateModel = function () {
-            /*
-             defer handling mouseup event until $scope.signaturePad handles
-             first the same event
-             */
-            $timeout().then(function () {
+            $timeout(function(){
               $scope.dataurl = $scope.signaturePad.isEmpty() ? EMPTY_IMAGE : $scope.signaturePad.toDataURL();
+              $scope.fieldForm[$scope.fieldId] = $scope.dataurl;
             });
           };
 
           $scope.clear = function () {
+            delete $scope.fieldForm[$scope.fieldId];
             $scope.signaturePad.clear();
             $scope.dataurl = EMPTY_IMAGE;
           };
 
-          $scope.$watch("dataurl", function (dataUrl) {
+          $scope.$watch('dataurl', function (dataUrl) {
             if (!dataUrl || $scope.signaturePad.toDataURL() === dataUrl) {
               return;
             }
@@ -66,60 +79,63 @@ angular.module('signature').directive('signaturePad', ['$interval', '$timeout', 
           });
         }
       ],
-      link: function (scope, element, attrs) {
+      link: function (scope, element) {
+        scope.width = $('.signatureWrapper').width()-26;
+
         var canvas = element.find('canvas')[0];
         var parent = canvas.parentElement;
         var scale = 0;
         var ctx = canvas.getContext('2d');
 
-        var width = parseInt(scope.width, 10);
+        var width = parseInt(scope.width-26, 10);
         var height = parseInt(scope.height, 10);
 
         canvas.width = width;
         canvas.height = height;
 
-        scope.signaturePad = new SignaturePad(canvas);
+        scope.signaturePad = new SignaturePad(canvas);// jshint ignore:line
 
         scope.setDataUrl = function(dataUrl) {
           var ratio = Math.max(window.devicePixelRatio || 1, 1);
-
           ctx.setTransform(1, 0, 0, 1, 0, 0);
           ctx.scale(ratio, ratio);
 
           scope.signaturePad.clear();
           scope.signaturePad.fromDataURL(dataUrl);
 
-          $timeout().then(function() {
+          $timeout(function() {
             ctx.setTransform(1, 0, 0, 1, 0, 0);
             ctx.scale(1 / scale, 1 / scale);
           });
         };
 
         scope.$watch('disabled', function (val) {
-            val ? scope.signaturePad.off() : scope.signaturePad.on();
+          if(val){
+            scope.signaturePad.off();
+          }else{
+            scope.signaturePad.on();
+          }
         });
-        
-        var calculateScale = function() {
-          var scaleWidth = Math.min(parent.clientWidth / width, 1);
-          var scaleHeight = Math.min(parent.clientHeight / height, 1);
 
-          var newScale = Math.min(scaleWidth, scaleHeight);
+        var calculateScale = function() {
+          var newScale = Math.min(parent.clientWidth / width, 1);
 
           if (newScale === scale) {
             return;
           }
 
           var newWidth = width * newScale;
-          var newHeight = height * newScale;
-          canvas.style.height = Math.round(newHeight) + "px";
-          canvas.style.width = Math.round(newWidth) + "px";
+          //var newHeight = height * newScale;
+          //canvas.style.height = Math.round(newHeight) + 'px';
+          canvas.style.width = Math.round(newWidth) + 'px';
 
           scale = newScale;
           ctx.setTransform(1, 0, 0, 1, 0, 0);
-          ctx.scale(1 / scale, 1 / scale);
+          ctx.scale(1 / scale, 1/ scale);
         };
 
         var resizeIH = $interval(calculateScale, 200);
+
         scope.$on('$destroy', function () {
           $interval.cancel(resizeIH);
           resizeIH = null;
@@ -136,10 +152,6 @@ angular.module('signature').directive('signaturePad', ['$interval', '$timeout', 
         element.on('touchend', onTouchend);
 
         function onTouchstart(event) {
-          scope.$apply(function () {
-            // notify that drawing has started
-            scope.notifyDrawing({ drawing: true });
-          });
           event.preventDefault();
         }
 
@@ -149,7 +161,6 @@ angular.module('signature').directive('signaturePad', ['$interval', '$timeout', 
             scope.updateModel();
 
             // notify that drawing has ended
-            scope.notifyDrawing({ drawing: false });
           });
           event.preventDefault();
         }
